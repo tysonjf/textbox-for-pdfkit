6f38bedaa34f2f9ae529c6d01698bfd8d9908559
Tyson <100465135+DieMantra@users.noreply.github.com>
2025-05-27 20:31:36 +1000

    updated vite config and package exports

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	build: {
 		lib: {
-			entry: resolve(__dirname, 'src/pdfTextbox.ts'),
-			name: 'TextboxForPdfkit',
+			entry: resolve(__dirname, 'src/index.ts'),
+			name: 'pdfTextbox',
 			fileName: (format) => `pdfTextbox.${format}.js`,
 			formats: ['es', 'cjs', 'umd'],
 		},
